d0fb8fab3a23f9e367d0daef3589405f53a910af
Msafiri Mnyasa <msafiri.athumani@wcf.go.tz>
2020-04-16 22:35:03 +0300

    change folder names

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['namespace'=>'Msafiri\RolesReports\Http\Controllers'],function(){
+Route::group(['namespace'=>'Msafiri\RolesReports\Http\controllers'],function(){
 	Route::get('user_role_reports','RolesReportController@reports');
 });
